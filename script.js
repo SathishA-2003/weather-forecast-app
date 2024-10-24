a56d4b8f9e5fd18360ff4f0d10f3512a4632cbb7
SathishA-2003 <sathishanbarasan3@gmail.com>
2024-10-24 16:36:29 +0530

    adding js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const temp = document.getElementById("temp"),
     date = document.getElementById("date-time"),
 currentLocation = document.getElementById('location'),
-condition = document.getElementById("condition");
+condition = document.getElementById("condition"),
+rain = document.getElementById("rain"),
+mainIcon = document.getElementById("icon"),
+uvIndex = document.querySelector('.uv-index'),
+uvText = document.querySelector('.uv-text'),
+windSpeed = document.querySelector('.wind-speed'),
+sunRise = document.querySelector('.sun-rise'),
+sunSet = document.querySelector('.sun-set'),
+humidity = document.querySelector('.humidity'),
+visibility = document.querySelector('.visibility'),
+humidityStatus = document.querySelector('.humidity-status'),
+airQuality = document.querySelector('.air-quality'),
+airQualitystatus = document.querySelector('.air-quality-status'),
+visibilityStatus = document.querySelector('.visibility-status');
 
 let currentCity = "";
 let currentUnit = "c";
@@ -78,5 +91,15 @@ function getWeatherData(city, unit, hourlyorWeek){
        }
        currentLocation.innerText = data.resolvedAddress;
         condition.innerText = today.conditions;
+        rain.innerText = "perc - " + today.precip + "%";
+        uvIndex.innerText = today.uvindex;
+        windSpeed.innerText = today.windspeed;
+        humidity.innerText = today.humidity + "%";
+        visibility.innerText = today.visibility;
+        airQuality.innerText = today.winddir;
     });
 };
+
+function celciusToFahrenheit(temp){
+    return ((temp * 9) / 5 + 32).toFixed(1);
+}
